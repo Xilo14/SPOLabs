9ab1e1a5000b80c9caf08e2f71629219cd50425b
Xilosof <pro100temi444@gmail.com>
2020-03-15 20:38:06 +0300

    Init dvornik folder, init lab2 folder,inuit lab2 projects

---
+++ spo2_child/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spo2_child
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
