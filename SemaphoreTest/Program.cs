edb874ed1879b0e3dd36bddcf092b8ac3ba19a29
Xilosof <pro100temi444@gmail.com>
2020-04-26 22:20:59 +0300

    Fixed Example code for remove cycle and counter

--- SemaphoreTest/Program.cs
+++ SemaphoreTest/Program.cs
@@ -18,7 +18,7 @@ namespace SemaphoreTest
     {
         static Semaphore sem = new Semaphore(3, 3);        // создаем семафор
         Thread myThread;
-        int count = 3;// счетчик чтения         
+        //int count = 3;// счетчик чтения         
         public Reader(int i)
         {
             myThread = new Thread(Read);
@@ -28,16 +28,16 @@ namespace SemaphoreTest
 
         public void Read()
         {
-            while (count > 0)
-            {
-                sem.WaitOne();
-                Console.WriteLine("{0} в библиотеке", Thread.CurrentThread.Name);
-                Thread.Sleep(1000);
-                Console.WriteLine("{0} покидает библиотеку", Thread.CurrentThread.Name);
-                sem.Release();
-                count--;
-                Thread.Sleep(1000);
-            }
+            //while (count > 0)
+            //{
+            sem.WaitOne();
+            Console.WriteLine("{0} в библиотеке", Thread.CurrentThread.Name);
+            Thread.Sleep(1000);
+            Console.WriteLine("{0} покидает библиотеку", Thread.CurrentThread.Name);
+            sem.Release();
+            //   count--;
+            Thread.Sleep(1000);
+            //}
         }
     }
 }
