f49743f8ec4cfade027b2b6a6c714902c2d0555d
Юрий Дьяченко <yurokke.yury@gmail.com>
2020-04-26 23:51:06 +0300

    created 3 lab

---
+++ lab3EventStarter/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lab3EventProducer
+{
+    class Program
+    {
+        static int countProducts = 0;
+        static EventWaitHandle wh = new EventWaitHandle(false, EventResetMode.AutoReset, "OOO'РОГА И КОПЫТА'");
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(Produce);
+            thread.Start();
+            while (true)
+            {
+                //#мама спаси меня
+
+            }
+
+        }
+
+        static void Produce()
+        {
+            while(true)
+            {
+                Thread.Sleep(5000);
+                countProducts++;               
+            }
+        }
+    }
+}
---
+++ lab3EventStarter/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("lab3EventStarter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("lab3EventStarter")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("374257d9-0f94-4d9b-8f4a-e544d718e9b4")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
