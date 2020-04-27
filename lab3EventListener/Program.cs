1943afb9e6111bee242bda30baa3906d7bc4529b
Юрий Дьяченко <yurokke.yury@gmail.com>
2020-04-27 12:06:55 +0300

    Ready lab3 Dyachenko

--- lab3EventListener/Program.cs
+++ lab3EventListener/Program.cs
@@ -2,14 +2,55 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace lab3EventConsumer
 {
     class Program
     {
+        static int countProducts = 0;
+        static EventWaitHandle wh = new EventWaitHandle(false, EventResetMode.AutoReset, "OOO'РОГА И КОПЫТА'");
         static void Main(string[] args)
         {
+            Thread thread1 = new Thread(Consume);
+            thread1.Start();
+            Thread thread2 = new Thread(Shop);
+            thread2.Name = "Shop";
+            thread2.Start();
+        }
+        static void Shop()
+        {
+            while (true)
+            {
+                wh.WaitOne();
+                Console.WriteLine(Thread.CurrentThread.Name + " получил товар");
+                countProducts++;
+            }
+        }
+        static void Consume()
+        {
+            while (true)
+            {
+                Console.WriteLine("Ожидание продукта");
+                while (true)
+                {
+                    ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                    if (keyInfo.Key == ConsoleKey.Enter)
+                    {
+                        if (countProducts >= 1)
+                        {
+                            countProducts--;
+                            Console.WriteLine("Продукт получен");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Нет доступных продуктов, ожидайте");
+                        }
+                    }
+                }
+            }
         }
     }
+
 }
--- lab3EventStarter/Program.cs
+++ lab3EventStarter/Program.cs
@@ -9,27 +9,16 @@ namespace lab3EventProducer
 {
     class Program
     {
-        static int countProducts = 0;
+        
         static EventWaitHandle wh = new EventWaitHandle(false, EventResetMode.AutoReset, "OOO'РОГА И КОПЫТА'");
         static void Main(string[] args)
         {
-            Thread thread = new Thread(Produce);
-            thread.Start();
             while (true)
             {
-                //#мама спаси меня
-
-            }
-
-        }
-
-        static void Produce()
-        {
-            while(true)
-            {
                 Thread.Sleep(5000);
-                countProducts++;               
+                Console.WriteLine("Продукт произведен");
+                wh.Set();
             }
-        }
+        }        
     }
 }
